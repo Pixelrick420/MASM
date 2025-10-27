768d4b96a8833c1951a85972735df04155a9e9f0
Harikrishnan R <harikrishnanr0505@gmail.com>
2025-10-27 14:58:32 +0530

    rev fix

--- ASM/rev.asm
+++ ASM/rev.asm
@@ -18,10 +18,6 @@ CODE SEGMENT
         MOV SP, 128
         JMP BACK
 
-    SWAP:
-        XCHG AL, BL
-        JMP INNER_BACK
-
     PRINT_AL:
         ADD AL, '0'
         MOV DL, AL
@@ -40,33 +36,31 @@ CODE SEGMENT
         JNZ PRINT_LOOP
         RET
 
-    INNER:
-        MOV CL, CH
-        MOV SI, OFFSET array
-    INNER_LOOP:
-        MOV AL, [SI]
-        MOV BL, [SI + 1]
-        CMP BL, AL
-        JBE SWAP
-    INNER_BACK:
-        MOV [SI], AL
-        MOV [SI + 1], BL
-        INC SI
-        DEC CL
-        JNZ INNER_LOOP
-        RET
 
     START:
         JMP INIT
     BACK:
         CALL PRINT_ARRAY
-    OUTER:
-        MOV CH, n
-        DEC CH    
-    OUTER_LOOP:
-        CALL INNER
-        DEC CH
-        JNZ OUTER_LOOP
+
+        MOV SI, OFFSET array
+        MOV DI, OFFSET array
+        MOV CL, n
+        MOV CH, 00H
+        ADD DI, CX
+        DEC DI
+
+    _LOOP:
+        CMP DI, SI
+        JBE STOP
+
+        MOV AL, [SI]
+        XCHG AL, [DI]
+        MOV [SI], AL
+
+        INC SI
+        DEC DI
+        JMP _LOOP
+
     STOP:
         CALL PRINT_ARRAY
         MOV AH, 4CH
@@ -75,37 +69,3 @@ CODE SEGMENT
 CODE ENDS
 END START
 
-COMMENT @@@
-MEMORY:
-0800 05H  
-0801 04H 
-0802 00H 
-0803 01H
-0804 06H
-0900 05H 
-
-
-CODE:
-0600     MOV CH, [0900]
-0604     DEC CH
-0606     CALL 0613
-0609     DEC CH
-060B     JNZ 0606
-060D     HLT
-
-060E     XCHG AL, BL
-0610     JMP 0621
-
-0613     MOV CL, CH
-0615     MOV SI, 0800
-0618     MOV AL, [SI]
-061A     MOV BL, [SI + 1]
-061D     CMP BL, AL
-061F     JBE 060E
-0621     MOV [SI], AL
-0623     MOV [SI + 1], BL
-0626     INC SI
-0627     DEC CL
-0629     JNZ 0618
-062B     RET
-@@@
